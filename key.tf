da06ed59398f4fee01fed0585eac1a7aa342e330
mohankumarsg <mohankumarsg.it@gmail.com>
2025-03-19 18:05:22 +0530

    terravrom

--- key.tf
+++ key.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "ec2_ex2" {
-  ami           = var.myami
-  instance_type = var.instance_type
-  count = var.instance_count
-  key_name = "mohankey"
+  ami                         = var.myami
+  instance_type               = var.instance_type
+  count                       = var.instance_count
+  key_name                    = "mohankey"
   associate_public_ip_address = var.enable_public_ip
 
 
   tags = {
-    Name = "mohaneterraform"
+    Name = "mohaneterraform1"
   }
 }
 
